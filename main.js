65f909e83881bceb59027425fdfb0e02d8f45b91
moyy <moyy@qq.com>
2021-02-06 18:26:44 +0800

    feat: 随机 mangle & 注入死代码

--- main.js
+++ main.js
@@ -25,3 +25,62 @@ async function infer_options(options) {
         return error.defs;
     }
 }
+
+// (async function () {
+//     let src = readFileSync("gui.asm.js", "utf8");
+//     let {
+//         code
+//     } = await minify(src, {
+//         shuffle_mangle: "ydzm",
+//         mangle: true,
+//         keep_classnames: false,
+//         compress: false,
+//     });
+
+//     writeFileSync("gui.js", code, "utf-8");
+// })();
+
+(async function () {
+
+    let src = `
+
+    function abcdefg(abc, def) {
+        let aaa = 13;
+        let bbb = 12;
+
+        const abcdefg = (abc, dee) => {
+            let ccc = 15;
+            let bbb = 17;
+
+            console.log("aaa = " + aaa);
+        }
+
+        aaa += bbb;
+
+        return aaa * 3;
+    }
+    `;
+
+    let r = new Set();
+
+    for (let i = 0; i < 10; ++i) {
+        let {
+            code
+        } = await minify(src, {
+            shuffle_mangle: "ydzm" + i,
+            
+            mangle: true,
+            keep_classnames: false,
+            compress: false,
+        });
+        
+        r.add(code);
+    }
+
+    for (let c of r) {
+        console.log(c);
+        // break;
+    }
+
+    console.log(r.size);
+})();
